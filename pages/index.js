f3d24c57f294e73153a88452b3054e9f712ecf2f
Berlinaten <80010558+Berlinaten@users.noreply.github.com>
2022-05-31 16:48:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import { sortByDate } from '../utils'
 export default function Home({ posts }) {
   return (
       <div>
+    ja
           <Head>
               <title>Sales und Markteing</title>
           </Head>
