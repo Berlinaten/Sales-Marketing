5eabf9316f8e7f1638731aa339dfd2a47dccfd6a
Berlinaten <80010558+Berlinaten@users.noreply.github.com>
2022-05-31 16:28:55 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,7 @@ export default function Header() {
         <meta name='robots' content='index, follow />
         <meta property="og:type" content="website" />
         <meta property="og:image:type" content="image/jpeg" />
+        <meta name="google-site-verification" content="dOpGFO0v8SZbZaXAhQ13eR31E1eHX0MgSeOG54r_Hws" />
         
         <header>
             <div className='container'>
