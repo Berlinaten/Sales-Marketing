0ce5d0014aa9ed12199da0256884a0270052c5ff
Berlinaten <80010558+Berlinaten@users.noreply.github.com>
2022-06-01 21:59:14 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,6 @@ import Link from 'next/link'
 
 export default function Header() {
     return (
-        <meta name="google-site-verification" content="dOpGFO0v8SZbZaXAhQ13eR31E1eHX0MgSeOG54r_Hws" />
         <header>
             <div className='container'>
                 <Link href='/' passHref>
