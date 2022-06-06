3c40eea55acae91404f4e97650a051dfa8f8db41
Berlinaten <80010558+Berlinaten@users.noreply.github.com>
2022-06-06 16:03:16 +0200

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -11,7 +11,7 @@ const config = {
   // Default transformation function
   transform: async (config, path) => {
     return {
-      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      loc: path, https://sales-marketing.pages.dev/sitemap.env<config.siteUrl>/<path>
       changefreq: config.changefreq,
       priority: config.priority,
       lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
